3f6a0046f0a822e55d43d995871268f62c3f567c
BerniSc <bernhard.schoeffel@gmail.com>
2026-01-01 18:37:46 +0100

    Added Option for spikes to start reversed

--- Enemies/Spikes/spikes.gd
+++ Enemies/Spikes/spikes.gd
@@ -6,10 +6,13 @@ extends Area2D
 # TODO IMPLEMENT
 @export var extend_speed: float = 200.0
 
+@export var start_extended := false
+
 # Store original position
 var base_position: Vector2
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var shader = sprite.material as ShaderMaterial
+@onready var sprite_height = sprite.texture.get_height()
 
 signal touched_spike(body)
 
@@ -17,36 +20,42 @@ func _ready():
 	add_to_group("spikes")
 	base_position = position
 	body_entered.connect(_on_body_entered)
-
-func _on_extend_spikes_():
-	# Instantly move for now; later animate
-	position = base_position + Vector2(0, -extend_distance)
+	# Create unique material for this spike instance, otherwise they share uniforms
+	# Then everything sucks
+	if sprite.material:
+		sprite.material = sprite.material.duplicate()
+		shader = sprite.material as ShaderMaterial
+	reset_spikes()
 
 func _on_body_entered(body):
 	emit_signal("touched_spike", body)
 
 func reset_spikes():
-	position = base_position
 	scale.y = 1.0
-
-	if shader:
-		shader.set_shader_parameter("extension", 0.0)
+	position = base_position
+	if start_extended:
+		extend()
+	else:
+		retract()
 
 func extend():
-	var h = sprite.texture.get_height()
-	scale.y = 1.0 + (extend_distance / h)
-	position.y = base_position.y - h * (scale.y - 1)
+	scale.y = 1.0 + (extend_distance / sprite_height)
+	position.y = base_position.y - sprite_height * (scale.y - 1.5)
+	shader.set_shader_parameter("extension", extend_distance)
 
 func retract():
 	scale.y = 1.0
 	position.y = 0
 	position = base_position
+	shader.set_shader_parameter("extension", 0.0)
 
 func _on_change_spikestate(new_state):
-	if new_state == 1:
+	# Invert logic if start_extended is true
+	var extended = new_state == 1.0
+	if start_extended:
+		extended = not extended
+
+	if extended:
 		extend()
 	else:
 		retract()
-
-	if shader:
-		shader.set_shader_parameter("extension", extend_distance if new_state == 1 else 0.0)
