5644ecebcb54c19f7af8dafdb598211a35008e12
BerniSc <bernhard.schoeffel@gmail.com>
2026-01-04 16:59:28 +0100

    Implemented Triggerzone

---
+++ Interactable/trigger_zone.gd
@@ -0,0 +1,26 @@
+extends BaseInteractable
+
+@export var player_trigger_threshhold := 1
+
+var _player_cnt := 0
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		_player_cnt += 1
+		if _player_cnt >= player_trigger_threshhold:
+			print("Trigger")
+			set_state(true)
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		_player_cnt -= 1
+		if _player_cnt < player_trigger_threshhold:
+			set_state(false)
+
+func set_state(new_state: bool):
+	current_state = 1.0 if new_state else 0
+	state_changed.emit(current_state)
